ec713f60220c2e968b611560219c78fceb2a1097
Max J. Polster <max.polster@id-p.de>
2019-07-11 20:29:46 +0200

    Simplify validation.

--- test/observable-set.ts
+++ test/observable-set.ts
@@ -2,32 +2,15 @@ import test from "ava";
 import { ObservableSet, PatchObservable, PatchObservableLike, unitsToArray } from "../src";
 import { validatePatch } from "../src/testing/patch-observable";
 
-function validate<T>(source: PatchObservableLike<T>) {
-	let subscribed = false;
-	return new PatchObservable<T>(observer => {
-		return source.patches({
-			patch(patch) {
-				validatePatch(patch);
-				observer.patch(patch);
-			},
-			reject: error => observer.reject(error)
-		});
-	}, () => {
-		if (subscribed) {
-			throw new Error("validate can only be piped once.");
-		}
-		subscribed = true;
-	});
-}
-
 test("patches", t => {
 	t.plan(32);
 	const set = new ObservableSet<number>();
 
-	set.pipe(validate).pipe(unitsToArray).subscribe(arr => {
+	set.patches(validatePatch);
+	set.pipe(unitsToArray).subscribe(arr => {
 		t.deepEqual(arr, Array.from(set));
 	});
-	set.pipe(validate).pipe(unitsToArray).subscribe(arr => {
+	set.pipe(unitsToArray).subscribe(arr => {
 		t.deepEqual(arr, Array.from(set));
 	});
 
