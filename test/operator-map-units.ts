eee5adecd62805495f9eea686668a090ec4a5978
Max J. Polster <max.polster@id-p.de>
2019-07-13 10:43:23 +0200

    Validate unit map patches

--- test/operator-map-units.ts
+++ test/operator-map-units.ts
@@ -1,35 +1,41 @@
 import test from "ava";
-import { mapUnits, PatchObservable, Unit, unitsToArray } from "../src";
+import { mapUnits, PatchObservable, PatchObserver, Unit, unitsToArray } from "../src";
+import { validatePatch } from "../src/testing/patch-observable";
 
 test("patch processing", t => {
 	const events = [];
-	const x = new PatchObservable<number>(observer => {
-		const a: Unit<number> = { prev: null, next: null, value: 7 };
-		observer.patch({ prev: null, next: null, stale: null, fresh: { next: a, prev: a } });
+	let observer: PatchObserver<number>;
+	const units = new PatchObservable<number>(o => {
+		observer = o;
+	}).pipe(mapUnits, (value: number) => value * 2);
+	units.patches(validatePatch);
+	units.pipe(unitsToArray).subscribe(event => events.push(event));
 
-		const b: Unit<number> = { prev: a, next: null, value: 11 };
-		a.next = b;
-		observer.patch({ prev: a, next: null, stale: null, fresh: { next: b, prev: b } });
+	const a: Unit<number> = { prev: null, next: null, value: 7 };
+	observer.patch({ prev: null, next: null, stale: null, fresh: { next: a, prev: a } });
 
-		a.next = null;
-		observer.patch({ prev: a, next: null, stale: { next: b, prev: b }, fresh: null });
+	const b: Unit<number> = { prev: a, next: null, value: 11 };
+	a.next = b;
+	observer.patch({ prev: a, next: null, stale: null, fresh: { next: b, prev: b } });
 
-		const c: Unit<number> = { prev: null, next: a, value: 13 };
-		a.prev = c;
-		observer.patch({ prev: null, next: a, stale: null, fresh: { next: c, prev: c } });
+	a.next = null;
+	observer.patch({ prev: a, next: null, stale: { next: b, prev: b }, fresh: null });
 
-		observer.patch({ prev: null, next: null, stale: { next: c, prev: a }, fresh: null });
+	const c: Unit<number> = { prev: null, next: a, value: 13 };
+	a.prev = c;
+	observer.patch({ prev: null, next: a, stale: null, fresh: { next: c, prev: c } });
 
-		observer.patch({ prev: null, next: null, stale: null, fresh: null });
+	observer.patch({ prev: null, next: null, stale: { next: c, prev: a }, fresh: null });
 
-		const d: Unit<number> = { prev: null, next: null, value: 1 };
-		const e: Unit<number> = { prev: d, next: null, value: 3 };
-		d.next = e;
-		const f: Unit<number> = { prev: e, next: null, value: 5 };
-		e.next = f;
-		observer.patch({ prev: null, next: null, stale: null, fresh: { next: d, prev: f } });
+	observer.patch({ prev: null, next: null, stale: null, fresh: null });
 
-	}).pipe(mapUnits, (value: number) => value * 2).pipe(unitsToArray).subscribe(event => events.push(event));
+	const d: Unit<number> = { prev: null, next: null, value: 1 };
+	const e: Unit<number> = { prev: d, next: null, value: 3 };
+	d.next = e;
+	const f: Unit<number> = { prev: e, next: null, value: 5 };
+	e.next = f;
+	observer.patch({ prev: null, next: null, stale: null, fresh: { next: d, prev: f } });
+
 	t.deepEqual(events, [
 		[14],
 		[14, 22],
