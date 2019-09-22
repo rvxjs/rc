8b64b72fb5e7c36b04c7fceef40fd65dcd07b40a
Max J. Polster <max.polster@id-p.de>
2019-09-22 20:33:08 +0200

    Cleanup for new experimental implementation.

--- test/operators/to-promise.ts
+++
@@ -1,9 +0,0 @@
-import test from "ava";
-import { Observable, toPromise } from "../../src";
-
-test("resolve", async t => {
-	t.is(await new Observable<string>(observer => {
-		observer.resolve("foo");
-		observer.resolve("bar");
-	}).pipe(toPromise), "foo");
-});
